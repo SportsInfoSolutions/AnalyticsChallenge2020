f3cc9091a44b073a03cf205390acca109d792a4f
Michael Egle <mjegle@iastate.edu>
2020-06-26 23:06:39 -0500

    mike's additions to eda

--- eda.R
+++ eda.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(funModeling)
 library(ggridges)
-data <- read_csv("data/AnalyticsChallenge2020Data.csv")
+data <- read_csv(url('https://raw.githubusercontent.com/SportsInfoSolutions/AnalyticsChallenge2020/master/Data/AnalyticsChallenge2020Data.csv'))
 
 
 
@@ -23,6 +23,29 @@ summary(gap_lm)
 data %>% filter(EventType == "rush") %>%
   ggplot(aes(x = EPA, y = RunDirection, fill = UsedDesignedGap)) +
   geom_density_ridges(quantile_lines = TRUE, quantiles = c(0.5), alpha = 0.5)
+
+
+# Number of Rushers Listed vs EPA density ----------------------------------
+
+data %>%
+  filter(EventType == "rush" | EventType == "pass") %>%
+  group_by(GameID, EventID, EventType) %>%
+  summarize(qb_rushers = as.factor(sum(IsRushing)),
+            epa = head(EPA, 1)) %>%
+  ggplot(aes(x = epa, y = qb_rushers, fill = EventType)) +
+  geom_density_ridges(quantile_lines = TRUE, quantiles = c(0.5), alpha = 0.5)
+
+
+# Broken Plays ----------------------------------------------------------------
+
+data %>%
+  filter(str_detect(PlayDesc, "broken play"))
+
+data %>%
+  filter(RunDirection == "Middle") %>%
+  select(PlayDesc) %>%
+  filter(str_detect(PlayDesc, "left") | str_detect(PlayDesc, "right")) %>%
+  unique()
 
 
 
