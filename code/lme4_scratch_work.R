71c1fc060e95bf91968cf09de5a42b0f8a595825
Michael Egle <mjegle@iastate.edu>
2020-07-17 21:30:48 -0500

    messing around with simple models

--- code/lme4_scratch_work.R
+++ code/lme4_scratch_work.R
@@ -4,6 +4,8 @@ library(lme4)
 
 sis_df <- read.csv('https://raw.githubusercontent.com/SportsInfoSolutions/AnalyticsChallenge2020/master/Data/AnalyticsChallenge2020Data.csv', stringsAsFactors = F)
 
+# Added a couple of the features that Zach had mentioned
+
 # See how many players get pressure on one play
 sis_df %>%
   group_by(GameID, EventID) %>%
@@ -17,3 +19,12 @@ sis_df <- sis_df %>%
 sis_df <- sis_df %>%
   group_by(GameID, EventID) %>%
   mutate(ListedDefenders = n())
+
+# EPA is a character variable for some reason so let's change that
+sis_df$EPA <- as.numeric(sis_df$EPA)
+
+# Just kinda messing around with some simple models
+sis_model <- lmer(formula = EPA ~ 1 + total_pressure + (1|OnFieldPosition),
+                  data = sis_df)
+
+summary(sis_model)
